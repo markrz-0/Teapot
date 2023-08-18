16db48db8d70118ff35f7163a0c76b9d1c8702e8
Marcin Krzewinski <useless.boy.v@protonmail.com>
2023-08-18 19:56:01 +0200

    made prject more configurable; minor code style changes

--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
 #include <Windows.h>
 #include <ctime>
-#include "image_arr.hpp"
+#include "image_arr.hpp" 
+
+enum OperationMode {
+    SINGLE_CENTER, // single centered image
+    SINGLE_RANDOM, // single random location image (location decided at exe startup)
+    MULTIPLE_RANDOM // new image at new location every frame
+};
+
+// used for calculation where screen center is
+constexpr int SCREEN_WIDTH = 1920;
+constexpr int SCREEN_HEIGHT = 1080;
+
+constexpr int FPS = 15; // how many times a second should image be redrawn; higher values impact pc performance
+
+constexpr int OPERATION_MODE = OperationMode::MULTIPLE_RANDOM;
 
 
 void DrawImage(int start_x, int start_y) {
-    int img_heigth = IMG_COLOR_ARR_LENGTH / IMG_WIDTH;
     HBITMAP bitmap = CreateBitmap(
         IMG_WIDTH,
-        img_heigth,
+        IMG_HEIGHT,
         1, // Color Planes, unfortanutelly don't know what is it actually. Let it be 1
         32, // Size of memory for one pixel in bits (in win32 4 bytes = 4*8 bits)
         (void*) IMG_COLOR_ARR); // pointer to array
@@ -22,12 +35,12 @@ void DrawImage(int start_x, int start_y) {
         start_x,  // x and
         start_y,  // y - upper-left corner of place, where we'd like to copy
         IMG_WIDTH, // width of the region
-        img_heigth, // height
+        IMG_HEIGHT, // height
         src, // source
         0,   // x and
         0,   // y of upper left corner  of part of the source, from where we'd like to copy
         IMG_WIDTH, // width of the region
-        img_heigth, // height
+        IMG_HEIGHT, // height
         (UINT)RGB(0, 0, 255)); // Defined key transparency color
     
     DeleteObject(bitmap);
@@ -38,30 +51,41 @@ void DrawImage(int start_x, int start_y) {
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     PSTR lpCmdLine, int nCmdShow)
 {
-    int screenWidth = 1920;
-    int screenHeight = 1080;
 
-    int img_heigth = IMG_COLOR_ARR_LENGTH / IMG_WIDTH;
-
-    int start_x = (screenWidth - IMG_WIDTH) / 2;
-    int start_y = (screenHeight - img_heigth) / 2;
+    int start_x = (SCREEN_WIDTH - IMG_WIDTH) / 2;
+    int start_y = (SCREEN_HEIGHT - IMG_HEIGHT) / 2;
 
     int MAX_X = start_x * 2;
     int MAX_Y = start_y * 2;
 
-    int offset_x = 0;
-    int offset_y = 0;
-
     srand(time(NULL));
 
+    int offset_x = rand();
+    int offset_y = rand();
+
+    int sleep_time = 1000 / FPS;
+
     while(true) {
-        DrawImage(
-            (start_x + offset_x) % MAX_X,
-            (start_y + offset_y) % MAX_Y
-        );
-        offset_x = rand();
-        offset_y = rand();
-        Sleep(100);
+        switch (OPERATION_MODE) {
+        case OperationMode::SINGLE_CENTER:
+            DrawImage(start_x, start_y);
+            break;
+        case OperationMode::SINGLE_RANDOM:
+            DrawImage(
+                (start_x + offset_x) % MAX_X,
+                (start_y + offset_y) % MAX_Y
+            );
+            break;
+        case OperationMode::MULTIPLE_RANDOM:
+            DrawImage(
+                (start_x + offset_x) % MAX_X,
+                (start_y + offset_y) % MAX_Y
+            );
+            offset_x = rand();
+            offset_y = rand();
+            break;
+        }
+        Sleep(sleep_time);
     }
     
     return 0;
